0cbcb950eae7d6f70aaae1a77e84fcbb47ecafe0
MaksimNorvatkin <norvatkin_maksim@mail.ru>
2025-12-12 23:17:39 +0300

    Initial commit

---
+++ src/main/java/ru/top/dz_19http/Dz19httpApplication.java
@@ -0,0 +1,13 @@
+package ru.top.dz_19http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz19httpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz19httpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/top/dz_19http/Dz19httpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.top.dz_19http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz19httpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
